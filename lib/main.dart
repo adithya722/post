31d1cdc964d160b22d9926b6517eb377ac9a9154
adithya722 <adhuadithya500@gmail.com>
2024-10-17 10:22:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Post App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    PostScreen(contentType: 'text', content: 'This is a text post.'),
+    PostScreen(contentType: 'video', content: 'assets/sample-1.mp4'),
+    PostScreen(contentType: 'image', content: 'assets/computer.jpg'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Simple Post App')),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.text_fields), label: 'Text'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_label), label: 'Video'),
+          BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Image'),
+        ],
+      ),
+    );
+  }
+}
+
+class PostScreen extends StatelessWidget {
+  final String contentType;
+  final String content;
+
+  PostScreen({required this.contentType, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  if (contentType == 'text') 
+                    Text(content, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                  if (contentType == 'video') 
+                    VideoPost(videoPath: content),
+                  if (contentType == 'image') 
+                    ImagePost(imagePath: content),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () {
+              final link = 'https://example.com/posts/$contentType';
+              Share.share('Check out this post: $link');
+            },
+            icon: Icon(Icons.share, size: 18),  // Reduced icon size
+            label: Text('Share'),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VideoPost extends StatefulWidget {
+  final String videoPath;
+
+  VideoPost({required this.videoPath});
+
+  @override
+  _VideoPostState createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: SizedBox(
+                height: 200, // Reduced video height
+                width: double.infinity,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          )
+        : Center(child: CircularProgressIndicator());
+  }
+}
+
+class ImagePost extends StatelessWidget {
+  final String imagePath;
+
+  ImagePost({required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.fill, // Make the image fit properly
+        height: 500, // Reduced image height
+        width: double.infinity,
+      ),
+    );
+  }
+}
